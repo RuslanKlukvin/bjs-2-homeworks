aafc609c0de811ac773c49edf2e1b359cc5c42c6
RuslanKlukvin <i-s-istfaka@yandex.ru>
2023-04-12 21:54:05 +0700

    [200~

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+	let arr = [];
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	let d = b**2 -4 * a * c;
   
-}
+	if (d > 0)
+		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+	if (d == 0)
+		arr = [-b / (2 * a)];
+	if (d < 0)
+		arr = [];
+	return arr;
+  }
+  
+  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
+	  return false;
+	}
+	let p = parseFloat(percent) / 100 / 12;
+	let monthPayment = (amount - contribution) * (p + (p / (Math.pow(1 + p, countMonths) - 1)));
+	return Math.round(monthPayment * countMonths * 100) / 100;
+  }
