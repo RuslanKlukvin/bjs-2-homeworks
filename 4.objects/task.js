6dae74542e6451b82576fb43cac9f3c3f01a7ef2
RuslanKlukvin <i-s-istfaka@yandex.ru>
2023-04-22 12:27:11 +0700

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marks) {
+    console.log(marks in this);
+    if ("marks" in this) {
+      console.log(marks);
+      // this.marks = this.marks.concat(marks);
+      this.marks.push(...marks);
+    }
+  };
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+    if ("marks" in this) {
+      return this.marks.reduce((ac, v, i, arr) => ac + v / arr.length, 0);
+    } else {
+      return 0;
+    }
+  };
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
